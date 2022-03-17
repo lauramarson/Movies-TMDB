f54b91eedfc2258b73b331e0cc07f5263adb3a83
lauramarson <laurampinheiro17@gmail.com>
2022-03-17 16:21:57 -0300

    wip

---
+++ Movies/Movies/Movie.swift
@@ -0,0 +1,12 @@
+//
+//  Movies.swift
+//  Movies
+//
+//  Created by Laura Pinheiro Marson on 17/03/22.
+//
+
+import Foundation
+
+class Movie: Codable {
+    
+}
--- Movies/Movies/ViewController.swift
+++ Movies/Movies/ViewController.swift
@@ -9,12 +9,81 @@ import Alamofire
 import UIKit
 
 class ViewController: UITableViewController {
+    lazy var searchBar: UISearchBar = UISearchBar()
+    var movies = [Movie]()
+    
+    var searchedMovies = [Movie]()
+    var searching = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchBar.searchBarStyle = UISearchBar.Style.prominent
+        searchBar.placeholder = " Search..."
+        searchBar.sizeToFit()
+        searchBar.isTranslucent = false
+        searchBar.backgroundImage = UIImage()
+        searchBar.delegate = self
+        
+        let leftNavBarButton = UIBarButtonItem(customView:searchBar)
+        self.navigationItem.leftBarButtonItem = leftNavBarButton
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+            return searchedMovies.count
+        } else {
+            return movies.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Movie", for: indexPath)
+        // escrever detalhes da célula
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+}
 
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        let filtered = movies.filter { movie in
+//
+//        }
+        
+//        searchedMovies = filtered
+        searching = true
+        tableView.reloadData()
+        
+        if searchBar.text == "" {
+            searching = false
+            tableView.reloadData()
+        }
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        resetSearchbar(searchBar)
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        resetSearchbar(searchBar)
+    }
+    
+    func resetSearchbar(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searching = false
+        searchBar.text = ""
+        tableView.reloadData()
+        searchBar.resignFirstResponder()
+    }
 
 }
 
